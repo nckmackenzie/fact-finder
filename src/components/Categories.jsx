5b34d840b08f1a7edf670c527497b139979fb94b
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-12-21 10:50:17 +0300

    render select based on screen size

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,26 @@
 import { categories } from '../data/data';
+import useMediaQuery from '../hooks/use-media-query';
 import Category from './Category';
+import CategoriesSelect from './CategoriesSelect';
 function Categories() {
+  const cats = [{ name: 'all', color: '' }, ...categories];
+  const isMedium = useMediaQuery('(min-width: 768px)');
+
   return (
-    <aside className="flex gap-2 flex-col">
-      {categories?.map(category => (
-        <Category
-          key={category.name}
-          category={category.name}
-          bg={category.color}
-        />
-      ))}
-    </aside>
+    <>
+      {isMedium && (
+        <aside className="basis-1/12 flex gap-2 flex-col">
+          {cats?.map(category => (
+            <Category
+              key={category.name}
+              category={category.name}
+              bg={category.color}
+            />
+          ))}
+        </aside>
+      )}
+      {!isMedium && <CategoriesSelect categories={cats} />}
+    </>
   );
 }
 

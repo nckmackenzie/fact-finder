a9c1e92542222a6c3e6c5e47f99d307e600db814
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-12-21 14:42:39 +0300

    basic Content component

---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Fact from './Fact';
+
+export default function Content() {
+  return (
+    <div className="basis-11/12 flex flex-col gap-4">
+      <Fact />
+    </div>
+  );
+}

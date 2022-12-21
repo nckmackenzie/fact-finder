1be49a7ae398d8564e27602dee27b2ac804b309e
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-12-21 09:04:37 +0300

    add categories array

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+export const categories = [
+  { name: 'technology', color: '#3b82f6' },
+  { name: 'science', color: '#10b981' },
+  { name: 'finance', color: '#f43f5e' },
+  { name: 'society', color: '#eab308' },
+  { name: 'entertainment', color: '#ec4899' },
+  { name: 'health', color: '#14b8a6' },
+  { name: 'history', color: '#f97316' },
+  { name: 'news', color: '#8b5cf6' },
+];

b457ede0bdbf75e3510b5aa5073de9489b877a6f
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-12-21 11:20:25 +0300

    add inline padding

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,7 +4,7 @@ import Content from './Content';
 
 function MainContent() {
   return (
-    <div className="mt-4 md:mt-8 flex flex-col md:flex-row gap-8 md:max-h-[35rem]  overflow-scroll">
+    <div className="mt-4 md:mt-8 px-2 md:px-4 flex flex-col md:flex-row gap-8 md:max-h-[35rem] overflow-scroll">
       <Categories />
       <Content />
     </div>

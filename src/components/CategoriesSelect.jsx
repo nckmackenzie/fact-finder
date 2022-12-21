a8caf45f497ccdd263b19bee56845eb51afc1b1d
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-12-21 10:49:39 +0300

    reuseable select for categories

---
+++ src/components/CategoriesSelect.jsx
@@ -0,0 +1,24 @@
+import { useTheme } from '../context/ThemeProvider';
+
+function CategoriesSelect({ categories }) {
+  const { darkMode } = useTheme();
+  return (
+    <select
+      name="category"
+      id="category"
+      className={`input flex-1 ${darkMode ? 'bg-slate-700' : 'bg-stone-50'}`}
+      defaultValue=""
+    >
+      <option value="" disabled>
+        Select category
+      </option>
+      {categories?.map(category => (
+        <option value={category.name} key={category.name}>
+          {category.name.toUpperCase()}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export default CategoriesSelect;

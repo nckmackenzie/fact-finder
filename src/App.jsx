6f398ae7975977a758453af5bbdea1b336b92964
ncmackenzie <34639621+nckmackenzie@users.noreply.github.com>
2022-12-21 10:50:49 +0300

    add MainContent component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import LogoFormToggle from './components/LogoFormToggle';
 import Container from './components/Container';
 import { useTheme } from './context/ThemeProvider';
+import MainContent from './components/MainContent';
 function App() {
   const { darkMode } = useTheme();
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Container>
           <LogoFormToggle />
+          <MainContent />
         </Container>
       </main>
     </div>

e81b555cf46d8da12c672a98048546840034200b
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-12-23 19:36:07 +0300

    add values for url and alon key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://vhytkcuoinxkkvfxfzsb.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoeXRrY3VvaW54a2t2ZnhmenNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE2MjIyNjAsImV4cCI6MTk4NzE5ODI2MH0.NMNCgffaBNXZ6mTKmSEZTb3tuwW5Qc7jHo8Dh9lnA6k';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

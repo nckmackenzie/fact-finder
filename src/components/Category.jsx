b0c381b49ccddac4677076669f5437bae79203df
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-12-24 18:46:34 +0300

    set inline styles with dynamic bg color

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 function Category({ category, bg }) {
   return (
     <button
+      style={{ backgroundColor: [bg] }}
       type="button"
-      className={`btn btn-normal bg-[${bg}] ${
+      className={`btn btn-normal  ${
         category === 'all' ? 'btn-primary mb-4' : ''
       }`}
     >

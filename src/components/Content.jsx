a81f8669db8d0855120f3fa7b9220584dbcd849f
nckmackenzie <nicholas.mackenzie1@gmail.com>
2022-12-24 19:12:05 +0300

    load facts

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Fact from './Fact';
 
-export default function Content() {
+export default function Content({ isLoading, facts }) {
+  // console.log(facts);
   return (
     <div className="basis-11/12 flex flex-col gap-4">
-      <Fact />
+      {isLoading && <p>Loading...</p>}
+      {!isLoading &&
+        facts &&
+        facts.length > 0 &&
+        facts.map(fact => <Fact key={fact.id} {...fact} />)}
     </div>
   );
 }
